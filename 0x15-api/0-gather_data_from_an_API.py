a322edd9881f5a7aad397481f5ba18b10bc815dd
I-yan-u <iyanuajimobi12@gmail.com>
2023-04-25 01:30:13 +0100

    gather data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     completed = [t.get("title") for t in tasks if t.get("completed") is True]
 
-    print("Employee {} is done with tasks({}/{})"
+    print("Employee {} is done with tasks({}/{}):"
           .format(user.get("name"), len(completed), len(tasks)))
     for task in completed:
         print("\t {}".format(task))
