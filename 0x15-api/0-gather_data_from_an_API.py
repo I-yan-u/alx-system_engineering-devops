e027042b06a8a31cc3bc2e0630832a4816c658a5
I-yan-u <iyanuajimobi12@gmail.com>
2023-07-24 21:29:47 +0100

    gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Accessing API data"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+	url = 'https://jsonplaceholder.typicode.com/'
+	userId = sys.argv[1]
+	user = {"userId": userId}
+
+	req = requests.get(url + 'users/{}'.format(userId)).json()
+	req2 = requests.get(url + 'todos/', params=user).json()
+
+	#task_name = [todo.get('title') for todo in req2]
+	completed = [t.get('title') for t in req2 if t.get('completed') ==True]
+	user_name = req.get('name')
+
+	print('Employee {} is done with tasks ({}/{}):'
+		.format(user_name, len(completed), len(req2)))
+	[print('\t {}'.format(c)) for c in completed]
